efcd6ac45aa9dd0cefe5e3a7777aecc10d103573
Allan <altonallan47@gmail.com>
2023-03-29 09:27:29 +0200

    Update App.Js

---
+++ app.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector(".canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+const frameCount = 179;
+
+const currentFrame = (index) => `./best-ball/${(index + 1).toString()}.jpg`;
